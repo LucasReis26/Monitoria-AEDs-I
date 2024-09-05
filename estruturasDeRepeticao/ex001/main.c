22eda3bab5f3096564a9667d11836ac143a80cf4
Lucas Reis <luctreis@gmail.com>
2024-09-05 10:55:02 -0300

    Adicionando Comentários

--- estruturasDeRepeticao/ex001/main.c
+++ estruturasDeRepeticao/ex001/main.c
@@ -9,16 +9,21 @@
 
 int main()
 {
+	//Declaração de variáveis
 	int N;
 
+	//Capturando número para fazer a tabuada
 	printf("Digite o número para exibir a tabuada: ");
 	scanf("%d",&N);
-
+	
+	//for de 0 a 10 indicando por quais números serão multiplicados
 	for(int i = 0; i <= 10; i++)
 	{
+		//Se o índice for 0, printe de qual número faremos a tabuada
 		if(i == 0)
 			printf("\nVamos fazer a tabuada do %d!\n",N);
 		
+		//Processa dados e printa resultado com base no índice do for
 		printf("%d x %d = %d\n",N,i,(i * N));
 	}
 
